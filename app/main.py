328f8d809c1ffc8027b5167a01d25de8f92d5a18
Steve Chapman <steve.chapman@nzqa.govt.nz>
2023-04-12 19:58:24 +1200

    partial multi user changes

--- app/main.py
+++ app/main.py
@@ -10,41 +10,39 @@ def main():
     conn = connectToLdap(host,port,user,pw)
     now = pytz.utc.localize(datetime.now())
 
+    # users = getListOfUsers(conn,base='ou=Users2,o=dev')
+    # for user in users:
+    #     print(user)
+    #     userId = user
+    #     break
+
     # userId = input("enter userId : ")
     userId = "nzqaTPE174"
     userEntries = getUserEntries(conn,base='ou=Users2,o=dev',userId=userId)
+    
     uid = userEntries[0]['uid']
-    for entry in userEntries:
-        userRoleDns = getRoleDns(conn,base=entry.entry_dn)
-        userRoleAssociations = []
-        roleMappings = []
+    for entry in userEntries: # should only be one - need to warn when not
+        eqaContexts = getRoleDns(conn,base=entry.entry_dn)
+        
         roles = set()
-        for userRoleDn in userRoleDns:
-            roleDN = userRoleDn.entry_dn
-            # print()
+        for eqaContext in eqaContexts:
+            roleDN = eqaContext.entry_dn
+            roleAssociations = getRoleAssociation(conn,base=roleDN)
             
-            roleAssociationEntries = getRoleAssociation(conn,base=roleDN)
-            roleAssociation = roleAssociationEntries[0]['attributes']['member'][0]
-            startDate = roleAssociationEntries[0]['attributes']['startDate']
-            endDate = roleAssociationEntries[0]['attributes'].get('endDate')
+            roleAssociation = roleAssociations[0]['attributes']['member'][0]
+            startDate = roleAssociations[0]['attributes']['startDate']
+            endDate = roleAssociations[0]['attributes'].get('endDate')
             if (endDate):
                 if (startDate > now) or (endDate < now):
                     print(f"skipping roleDn: {roleDN} with startDate {startDate} and endDate {endDate}")
                     continue
-            # print()
-            # print(f" roleAssociation {roleAssociation}, startDate {startDate}, endDate {endDate}")
-            roleMappingEntries = getRoleMapping(conn,base=roleAssociation) #[0]#['member']
-            roleMapping = roleMappingEntries[0]['attributes']['member'][0] #.member
-            # print(f" roleMapping {roleMapping}")
-            roleEntries = getRole(conn,base=roleMapping)
-            role = roleEntries[0]['attributes']['cn'][0]
-            # print(f" role {role}")
-            roleMappings.append(roleMapping)
-            userRoleAssociations.append(roleAssociation)
+            roleMappings = getRoleMapping(conn,base=roleAssociation) 
+            
+            roleMapping = roleMappings[0]['attributes']['member'][0]
+            mappedRoles = getRole(conn,base=roleMapping)
+            role = mappedRoles[0]['attributes']['cn'][0]
+
             roles.add(role)
-            # exit = input("press any key to continue")
-            # if not exit:
-            #     break
         
     #  return the results 
     print(f"ESAA user {userId} with uid {uid} has the following roles: ")
@@ -68,7 +66,7 @@ def getUserEntries(conn,base,userId) -> list :
 def getListOfUsers(conn,base) -> list : 
     conn.search(search_base=base, 
                 search_filter='(sia-userid=*)',
-                attributes=['groupMembership','uid','securityEquals'])
+                search_scope=SUBTREE, attributes=ALL_ATTRIBUTES)
     print(f"User search returned {len(conn.entries)} entries")
     return conn.entries
 
@@ -94,25 +92,6 @@ def getRole(conn,base) -> dict :
     conn.search(search_base=base, 
                 search_filter='(objectClass=eqaRole2)', 
                 search_scope=SUBTREE, attributes=ALL_ATTRIBUTES)
-    return conn.response
-
-def getGroupsDetails(conn,username,base):
-    #  query for groups details
-    conn.search(search_base=base, 
-                search_filter='(cn='+username+')', 
-                attributes=['cn','sn','givenName','fullName','ou','o'])
-    print("Groups Details")
-    print(conn.response)
-    return conn.response
-
-def getRolesDetails(conn,username,base):
-    #  query for roles details
-    conn.search(search_base=base, 
-                search_filter='(cn='+username+')', 
-                # all attributes 
-                attributes=['cn','sn','givenName','fullName','ou','o'])
-    print("Roles Details")
-    print(conn.response)
     return conn.response
 
 if __name__ == '__main__':
